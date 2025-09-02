31e96e6311698976e8b48b8cf0dddf47cf855aa2
henry-jacq <henryeditz267@gmail.com>
2025-09-02 17:34:46 +0530

    refactor: remove deprecated camera and QR code scanning modules

--- scanner/sounds.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def play_sound(status: str):
-    if status == "success":
-        os.system("aplay sounds/success.wav")  # Success beep
-    elif status == "error":
-        os.system("aplay sounds/error.wav")    # Error beep
-    else:
-        os.system("aplay sounds/neutral.wav")  # Neutral beep
